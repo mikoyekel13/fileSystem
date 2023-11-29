95ba7d02939196e80182e6f2a63238ac6134553b
Ayeletg100 <ayeletginsberg@hilma.tech>
2023-11-29 15:28:31 +0200

    userspace fetch succeeded

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Login from "./components/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import UserSpace from "./components/UserSpace";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Login />} />
+          <Route path="/:username" element={<UserSpace />} />
         </Routes>
       </BrowserRouter>
     </>
--- client/src/components/UserSpace.jsx
+++ client/src/components/UserSpace.jsx
@@ -1,6 +1,24 @@
 import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
 function UserSpace() {
-  useEffect(() => {}, []);
+  let location = useLocation();
+  const currUser = location.pathname;
+  const [userFiles, setUserFiles] = useState([]);
+  async function loadFiles() {
+    try {
+      const resUserFiles = await fetch(`http://localhost:3000${currUser}`);
+      if (resUserFiles.status !== 200)
+        throw new Error("problem with importing the user's files");
+      const userFilesData = await resUserFiles.json();
+      console.log(userFilesData);
+      return userFilesData;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  useEffect(() => {
+    setUserFiles(loadFiles());
+  }, []);
 }
 
 export default UserSpace;
